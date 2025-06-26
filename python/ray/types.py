d4410164e95c46064dba85266f4634aa3ede7b4f
ANUSHREE1403 <anushree1422@gmail.com>
2025-06-26 23:52:29 +0530

    fix: unify ObjectRef type from ray._raylet and ray.types for compatibility (#54054)

--- python/ray/types.py
+++ python/ray/types.py
@@ -1,14 +1,17 @@
-from typing import Generic, TypeVar
-
+from typing import TYPE_CHECKING, TypeVar
 from ray.util.annotations import PublicAPI
 
-T = TypeVar("T")
+
 
 
 # TODO(ekl) this is a dummy generic ref type for documentation purposes only.
 # We should try to make the Cython ray.ObjectRef properly generic.
 # NOTE(sang): Looks like using Generic in Cython is not currently possible.
 # We should update Cython > 3.0 for this.
-@PublicAPI
-class ObjectRef(Generic[T]):
-    pass
+
+T = TypeVar("T")
+
+if TYPE_CHECKING:
+    from ray._raylet import ObjectRef as ObjectRef  # for static type checking
+else:
+    from ray._raylet import ObjectRef as ObjectRef  # for runtime usage

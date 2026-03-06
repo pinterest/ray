5a63649ba8f8ce13aabe16fab434763d13f9520d
lee1258561 <lee1258561@gmail.com>
2026-03-06 20:12:28 +0000

    test2

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,5 +1,5 @@
 # Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
+commit = "3c09330e767abb4fc25e0f11f153311ddaaca9b3"
 version = "2.52.1"
 
 if __name__ == "__main__":
